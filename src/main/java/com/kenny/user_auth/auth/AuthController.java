2ee17e4f971d915fb82963c0a025438fad56d544
kennyyiu <kenny.kwyiu@gmail.com>
2026-05-05 00:14:02 +0800

    AuthController

---
+++ src/main/java/com/kenny/user_auth/auth/AuthController.java
@@ -0,0 +1,33 @@
+package com.kenny.user_auth.auth;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+	private final UserRegistrationService userRegistrationService;
+
+	private final LoginService loginService;
+
+	private final SessionAuthSupport sessionAuthSupport;
+
+	public AuthController(
+			UserRegistrationService userRegistrationService,
+			LoginService loginService,
+			SessionAuthSupport sessionAuthSupport) {
+		this.userRegistrationService = userRegistrationService;
+		this.loginService = loginService;
+		this.sessionAuthSupport = sessionAuthSupport;
+	}
+
+
+}
