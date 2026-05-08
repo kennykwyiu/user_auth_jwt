b1f60d1b4706b4917290bad18ed4ff639a294537
kennyyiu <kenny.kwyiu@gmail.com>
2026-05-08 23:47:30 +0800

    SecurityConfig

--- src/main/java/com/kenny/user_auth/SecurityConfig.java
+++ src/main/java/com/kenny/user_auth/SecurityConfig.java
@@ -1,25 +1,18 @@
 package com.kenny.user_auth;
 
+import jakarta.servlet.http.HttpServletResponse;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http
-				.authorizeHttpRequests(auth -> auth
-						.requestMatchers("/api/hello").permitAll()
-						.anyRequest().authenticated())
-				.httpBasic(Customizer.withDefaults());
-
-		return http.build();
-	}
 
 }
